cf29f9660f4d31fee5fb837390bfc53735dfb5e7
Baixst <bawini97@yahoo.de>
2021-05-14 14:44:31 +0200

    added filter buttons

--- ticker_script.js
+++ ticker_script.js
@@ -1,3 +1,6 @@
+var sportOn = true;
+var spellOn = true;
+
 
 // function for forwards and backwards buttons of the ticker
 function forwardTicker() {
@@ -13,3 +16,38 @@ function resetTicker() {
     document.getElementById("ticker_marquee").setAttribute('scrollamount', 3);
     document.getElementById("ticker_marquee").setAttribute('direction', 'left');
 }
+
+
+// Filter Buttons
+function toggle_sportNews() {
+    if(sportOn) {
+        document.getElementById("sportFilter").setAttribute("class", "btn btn-dark");
+    }
+    else {
+        document.getElementById("sportFilter").setAttribute("class", "btn btn-info");
+    }
+
+    sportOn = !sportOn;
+}
+
+function toggle_spellNews() {
+    if(spellOn) {
+        document.getElementById("spellFilter").setAttribute("class", "btn btn-dark");
+    }
+    else {
+        document.getElementById("spellFilter").setAttribute("class", "btn btn-info");
+    }
+
+    spellOn = !spellOn;
+}
+
+/*
+function toggleButton() {
+    document.getElementById
+}
+
+function addFilter() {
+    document.getElementById("ticker_marquee").
+}
+
+*/
